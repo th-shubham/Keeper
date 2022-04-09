749a59f4ad6de78c6d9dd4f92d427d901587b132
shubham <shubham0jangid@gmail.com>
2022-04-09 18:03:37 +0530

    removed extra files git push
    git status

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,14 @@
 import React from "react";
 import Header from "./header";
 import Footer from "./footer";
-import notes from "./Notes";
+import CreateArea from "./CreateArea";
 import Note from "./Note";
 function App() {
 	return (
 		<div>
 			<Header />
-			{notes.map((noteitem) => (
-				<Note
-					key={noteitem.key}
-					title={noteitem.title}
-					content={noteitem.content}
-				/>
-			))}
+			<CreateArea />
+			<Note />
 			<Footer />
 		</div>
 	);

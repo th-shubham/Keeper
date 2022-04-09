34a701200469f90d342afde4c4a9a95fb0c16d49
shubham <shubham0jangid@gmail.com>
2022-04-09 21:08:27 +0530

    Added adding & display feature

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,19 @@ import CreateArea from "./CreateArea";
 import Note from "./Note";
 function App() {
 	const [notesArray, setNotesArray] = React.useState([]);
-	function addNote() {}
+	function addNote(noteToAdd) {
+		setNotesArray((prevNotesArray) => {
+			return [...prevNotesArray, noteToAdd];
+		});
+	}
 
 	return (
 		<div>
 			<Header />
 			<CreateArea onAdd={addNote} />
-			<Note />
+			{notesArray.map((noteItem, index) => {
+				return <Note title={noteItem.title} content={noteItem.content} />;
+			})}
 			<Footer />
 		</div>
 	);

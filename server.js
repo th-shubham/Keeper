d3b2ba382293c6ea1ca90e7bdba74ebff14348c6
shubham <shubham0jangid@gmail.com>
2022-04-19 11:55:17 +0530

    Ready to Deploy

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ connection.once("open", () => {
 const notesRouter = require("./routes/note");
 app.use(notesRouter);
 
+if (process.end.NODE_ENV == "production") {
+	app.use(express.static("client/build"));
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
 app.listen(port, () => {
 	console.log(`Server is running on Port ${port}`);
 });
